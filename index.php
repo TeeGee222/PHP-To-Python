8812dcbeaf50ea374b31d847cacd68f0b9f5a1ce
Thomas <75911858+TeeGee222@users.noreply.github.com>
2022-01-08 14:47:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOLA</title>
-</head>
-<body>
-    BONJOUR 
 <?php
-$hello = '150';
+$hello = '150'; //var that can be pass to python
 //run python
-echo shell_exec("python /laragon/www/Musiquetransfert/php_to_python_test/mkdir.py 2>&1" . $hello)
+echo shell_exec("python /link/to/your/python/file/mkdir.py 2>&1" . $hello)
 ?>
-</body>
-</html>
