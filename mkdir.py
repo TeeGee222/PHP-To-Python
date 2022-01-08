8d0cff0d6fa42d4bd89e8c281534ca9a93c2f8f2
Thomas <75911858+TeeGee222@users.noreply.github.com>
2022-01-08 14:52:55 +0100

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -4,3 +4,8 @@ import os
 import sys
 os.makedirs("Dossier crée avec python via un site web")
 print (sys.argv[1])
+
+#Get multiple var from php
+#arg1 = sys.argv[1]
+#arg2 = sys.argv[2]
+#arg3 = sys.argv[3]
