3d09fe4748b329eac9fee8488aac77fd44dff68f
Thomas <75911858+TeeGee222@users.noreply.github.com>
2022-01-08 14:48:51 +0100

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -1,4 +1,5 @@
 #!C:\Users\Administrateur\AppData\Local\Programs\Python\Python38-32 python
+#Change this to your python path
 import os 
 import sys
 os.makedirs("Dossier crée avec python via un site web")
