15672feeff3538769f156b45bf397cfff83ebe5d
Thomas <75911858+TeeGee222@users.noreply.github.com>
2022-01-08 14:54:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 $hello = '150'; //var that can be pass to python
 //run python
 echo shell_exec("python /link/to/your/python/file/mkdir.py 2>&1" . $hello)
+
+#pass multiple var to python
+#$info = $var1.' '.$var2.' '.$var3;
+#echo shell_exec("python /link/to/your/python/file/mkdir.py 2>&1" . $hello)
 ?>
